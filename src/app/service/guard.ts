819d5260904a2fe81847d5d47132ff7611e8c41c
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-12 19:29:18 +0530

    service guard.ts

--- src/app/service/guard.ts
+++ src/app/service/guard.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { Api } from './api';
+
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Guard {
-  
+
+export class Guard implements CanActivate {
+
+  constructor(private apiService: Api, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+     state: RouterStateSnapshot): boolean {
+
+    const requiresAdmin = route.data['requiresAdmin'] || false;
+
+    if (requiresAdmin) {
+      if (this.apiService.isAdmin()) {
+        return true;
+      }else{
+        this.router.navigate(['/login'], {
+          queryParams:{returnUrl: state.url}
+        });
+        return false;
+      }
+    }else{
+      if (this.apiService.isAuthenticated()) {
+        return true;
+      }else{
+        this.router.navigate(['/login'], {
+          queryParams:{returnUrl: state.url}
+        });
+        return false; //deny access
+      }
+    }
+  }
 }
