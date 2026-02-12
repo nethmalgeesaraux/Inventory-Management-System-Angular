421fd5d291846ebd49562ebdb3480581ef4cc1b1
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-12 21:21:08 +0530

    app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Api } from './service/api';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('Inventory-Management-System');
+
+
+export class App{
+  title = 'ims';
+  constructor(
+    private apiService: Api,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+
+isAuth():boolean{
+  return this.apiService.isAuthenticated();
+}
+
+isAdmin():boolean{
+  return this.apiService.isAdmin();
+}
+
+logOut():void{
+  this.apiService.logout();
+  this.router.navigate(["/login"])
+  this.cdr.detectChanges();
+}
+
+
+
+
+
 }
