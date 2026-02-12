075895182d9be3dc75b692501523a4fa16d12448
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-12 21:49:00 +0530

    register

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,11 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { Api } from '../service/api';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.css'
 })
+
 export class Register {
+  constructor(private apiService:Api, private router:Router){}
+
+  formData: any = {
+    email: '',
+    name: '',
+    phoneNumber: '',
+    password: ''
+  };
+  message:string | null = null;
+
+  async handleSubmit(){
+    if( 
+      !this.formData.email || 
+      !this.formData.name || 
+      !this.formData.phoneNumber || 
+      !this.formData.password 
+    ){
+      this.showMessage("All fields are required");
+      return;
+    }
+
+    try {
+      const response: any = await firstValueFrom(
+        this.apiService.registerUser(this.formData)
+      );
+      if (response.status === 200) {
+        this.showMessage(response.message)
+        this.router.navigate(["/login"]);
+      }
+    } catch (error:any) {
+      console.log(error)
+      this.showMessage(error?.error?.message || error?.message || "Unable to register a user" + error)
+      
+    }
+  }
+
+  showMessage(message:string){
+    this.message = message;
+    setTimeout(() =>{
+      this.message = null
+    }, 4000)
+  }
 
 }
