87e0f7d0d39bc636e5e263f53a1cdf94881faf55
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-13 11:01:50 +0530

    pagination

--- src/app/pagination/pagination.ts
+++ src/app/pagination/pagination.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './pagination.html',
-  styleUrl: './pagination.css',
+  styleUrl: './pagination.css'
 })
 export class Pagination {
+
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+  @Output() pageChange = new EventEmitter<number>;
+
+  //method to generate page numbers
+  get pageNumbers(){
+    return Array.from({length: this.totalPages}, (_, i)=> i+1);
+  }
+
+  //method to handle page change
+  onPageChange(page: number):void{
+    if (page >= 1 && page <= this.totalPages) {
+      this.pageChange.emit(page)
+    }
+  }
 
 }
