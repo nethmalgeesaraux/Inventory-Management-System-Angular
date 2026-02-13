14c4d46485e57056bdbe03650602d756d275d5be
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-13 11:50:00 +0530

    profile

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Api } from '../service/api';
+
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  styleUrl: './profile.css'
 })
-export class Profile {
+export class Profile implements OnInit {
+  constructor(private apiService: Api){}
+  user: any = null
+  message: string = "";
 
+  ngOnInit(): void {
+    this.fetchUserInfo();
+  }
+
+  fetchUserInfo():void{
+    this.apiService.getLoggedInUserInfo().subscribe({
+      next:(res)=>{
+        this.user = res;
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to Get Profile Info' + error
+        );
+      }
+    })
+  }
+
+  //SHOW ERROR
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
 }
